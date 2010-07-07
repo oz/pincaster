7be7cb5f893fc3f941ebaffb0dfc582d96e6d673
Arnaud Berthomier <arnaud@cyprio.net>
2010-07-07 17:06:05 +0200

    Use autoloader

--- lib/pincaster.rb
+++ lib/pincaster.rb
@@ -1,8 +1,9 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-require "pincaster/server"
-
 module Pincaster
   VERSION = '0.1'
+
+  autoload :Server, 'pincaster/server'
+  autoload :Layer, 'pincaster/layer'
 end
