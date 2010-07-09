6461918dd09b1d8ffce3427a71a69371a8639acc
Arnaud Berthomier <arnaud@cyprio.net>
2010-07-09 13:10:19 +0200

    Cleanup on Pincaster::Layer API

--- lib/pincaster/layer.rb
+++ lib/pincaster/layer.rb
@@ -1,31 +1,49 @@
-#!/usr/bin/env ruby
+require 'rest-client'
 
 class Pincaster::NoSuchLayer < Exception; end
 class Pincaster::LayerError < Exception; end
+class Pincaster::NoServer < Exception; end
 
 module Pincaster
   class Layer
 
-    attr_reader   :records, :geo_records, :server
-    attr_accessor :name, :type, :distance_accuracy, :latitude_accuracy, :longitude_accuracy, :bounds
+    @@accessors = %w(records geo_records server name type distance_accuracy latitude_accuracy longitude_accuracy bounds)
+    # Define accessors
+    @@accessors.each { |acs|    instance_eval "attr_accessor :#{acs}"  }
 
     # DEBUG
     attr_accessor :opts
 
     def initialize(opts={})
+      # DEBUG
       @opts = opts
-      @server = opts["server"] || opts[:server] || Pincaster::Server.new
-      @name = opts["name"] || opts[:name] || nil
-      @records = opts["records"] || opts[:records] || nil
+      # Set instance variables for each key/value in opts
+      @@accessors.each do |prop|
+        value = opts[prop] || opts[prop.to_sym]
+        send("#{prop}=", value) if value
+      end
+    end
+
+    # Define layer-type boolean reader such as:
+    # @example
+    #   layer = Pincaster::Layer.new :name => 'foo', :type => 'flat'
+    #   layer.flat? # true
+    #   layer.spherical? # false
+    %w(flat flatwrap spherical geoidal).each do |layer_type|
+      class_eval %Q(
+        def #{layer_type}?
+          @type == '#{layer_type}'
+        end
+      )
     end
 
     # Register a new layer
     # @return [Pincaster::Layer]
     def self.register(name, opts={})
-      server = opts[:server] || Pincaster::Server.new
-      response = server.post "layers/#{name}.json"
+      raise LayerError.new("no server set") unless opts[:server]
+      response = opts[:server].post "layers/#{name}.json"
       if response && %w(created existing).include?(response['status'])
-        Pincaster::Layer.new :name => name, :server => server
+        Pincaster::Layer.new :name => name, :server => opts[:server]
       else
         nil
       end
@@ -35,6 +53,7 @@ module Pincaster
     def register
       self.class.register @name, :server => @server
     end
+    alias :save :register
 
     # Delete a layer
     # @param [String] layer name
@@ -56,6 +75,7 @@ module Pincaster
         false
       end
     end
+    alias :destroy :delete
 
     # Delete a layer, raises exception on error
     #
@@ -68,11 +88,12 @@ module Pincaster
     def self.delete!(name, server)
       response = nil
       begin
+        raise "no server set" unless server
         response = server.delete "layers/#{name}.json"
       rescue RestClient::ResourceNotFound
         raise Pincaster::NoSuchLayer
       rescue => err
-        raise Pincaster::LayerError.new(response)
+        raise Pincaster::LayerError.new err.to_s
       end
       response
     end
@@ -81,6 +102,7 @@ module Pincaster
     def delete!
       self.class.delete! @name, @server
     end
+    alias :destroy! :delete!
 
     def to_s
       @name
--- spec/layer_spec.rb
+++ spec/layer_spec.rb
@@ -1,15 +1,19 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Pincaster::Layer" do
+  before(:all) do
+    @local_server = Pincaster::Server.new
+  end
+
   it "should instanciate cleanly" do
     Pincaster::Layer.new.should be_a Pincaster::Layer
   end
 
   it "should register a new layer" do
-    layer = Pincaster::Layer.new :name => 'a_new_layer'
+    layer = Pincaster::Layer.new :name => 'a_new_layer', :server => @local_server
     layer.register.should be_true
 
-    layer2 = Pincaster::Layer.register :name => 'a_new_layer'
+    layer2 = Pincaster::Layer.register 'a_new_layer', :server => @local_server
     layer2.should be_a Pincaster::Layer
   end
 
@@ -17,7 +21,13 @@ describe "Pincaster::Layer" do
     Pincaster::Server.new.layers.first.delete.should be_true
   end
 
+  it "should raise an exception when trying to create layers without a server" do
+    failing = lambda { Pincaster::Layer.new(:name => "foo").save }
+    failing.should raise_error Pincaster::LayerError
+  end
+
   it "should raise an exception when trying to delete non-existent layers" do
-    lambda { Pincaster::Layer.new(:name => "i_dont_exist").delete! }.should raise_error(Pincaster::NoSuchLayer)
+    failing = lambda { Pincaster::Layer.new(:name => "i_dont_exist", :server => @local_server).delete! }
+    failing.should raise_error Pincaster::NoSuchLayer
   end
 end
