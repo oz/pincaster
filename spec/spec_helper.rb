499a8ddb1c5bced0e50b282ec1b352ddb8718276
Arnaud Berthomier <arnaud@cyprio.net>
2010-07-15 10:36:19 +0200

    start only one pincaster instance for all the test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,33 +4,15 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
 require 'pincaster'
 require 'spec'
-#require 'spec/autorun'
 require 'fileutils'
 
-Spec::Runner.configure do |config|
-  config.before :suite do
-    @@pincaster_db        = '/tmp/pincaster_spec.db'
-    @@pincaster_conf_file = '/tmp/pincaster_spec.conf'
-    @@pincaster_server_port = 4270
-
-    cleanup_pincaster_files @@pincaster_db, @@pincaster_conf_file
-    setup_pincaster_config  @@pincaster_db, @@pincaster_conf_file, @@pincaster_server_port
-    @@pincaster_pid = startup_pincaster_daemon @@pincaster_conf_file
-    sleep 1  # wait a sec. to be sure your slow machine starts the process...
-  end
-
-  config.after :suite do
-    Process.kill 'TERM', @@pincaster_pid
-    Process.waitpid @@pincaster_pid
-    cleanup_pincaster_files @@pincaster_db, @@pincaster_conf_file
-  end
-end
+@@pincaster_db          = "/tmp/pincaster_spec_#{Process.pid}.db"
+@@pincaster_conf_file   = "/tmp/pincaster_spec_#{Process.pid}.conf"
+@@pincaster_server_port = 4269 + Process.pid
 
 # Cleanup Pincaster config and db file
 def cleanup_pincaster_files(*args)
-  args.each do |file|
-    File.delete(file) if File.exist? file
-  end
+  args.each { |file| File.delete(file) if File.exist?(file) }
 end
 
 # Write pincaster config file for rspec
@@ -49,11 +31,7 @@ end
 # Startup Pincaster daemon with an rspec config
 def startup_pincaster_daemon(config)
   Process.fork do
-    process = IO.popen "pincaster \"#{config}\" 2>&1"
-    trap("TERM") do
-      Process.kill "TERM", process.pid
-      Process.waitall
-    end
+    Process.exec 'pincaster', config
   end
 end
 
@@ -62,3 +40,12 @@ def pincaster_server_instance
   Pincaster::Server.new :host => '127.0.0.1', :port => @@pincaster_server_port
 end
 
+cleanup_pincaster_files @@pincaster_db, @@pincaster_conf_file
+setup_pincaster_config  @@pincaster_db, @@pincaster_conf_file, @@pincaster_server_port
+@@pincaster_pid = startup_pincaster_daemon @@pincaster_conf_file
+
+at_exit do
+  Process.kill 'TERM', @@pincaster_pid
+  Process.waitpid @@pincaster_pid
+  cleanup_pincaster_files @@pincaster_db, @@pincaster_conf_file
+end
